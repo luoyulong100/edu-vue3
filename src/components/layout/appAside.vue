21a12e7ca501aa69567f7e884564bf20b8e42661
luoyulong100 <17745132831@163.com>
2024-02-28 23:24:08 +0800

    feat

---
+++ src/components/layout/appAside.vue
@@ -0,0 +1,55 @@
+import type { setBlockTracking } from 'vue';
+<template>
+  <el-aside>
+    <el-menu>
+      <el-sub-menu>
+        <template #title>
+          <el-icon><IEpSetting /></el-icon> <span> 权限管理 </span>
+        </template>
+        <el-menu-item>
+          <el-icon><IEpSetting /></el-icon> <span> 菜单列表 </span>
+        </el-menu-item>
+        <el-menu-item>
+          <el-icon><IEpSetting /></el-icon> <span> 资源列表 </span>
+        </el-menu-item>
+        <el-menu-item>
+          <el-icon><IEpSetting /></el-icon> <span> 角色列表 </span>
+        </el-menu-item>
+      </el-sub-menu>
+
+      <el-menu-item>
+        <el-icon><IEpSetting /></el-icon> <span> 用户管理 </span>
+      </el-menu-item>
+
+      <el-menu-item>
+        <el-icon><IEpSetting /></el-icon> <span> 课程管理 </span>
+      </el-menu-item>
+
+      <el-sub-menu>
+        <template #title>
+          <el-icon><IEpSetting /></el-icon> <span> 广告管理 </span>
+        </template>
+        <el-menu-item>
+          <el-icon><IEpSetting /></el-icon> <span> 广告列表 </span>
+        </el-menu-item>
+        <el-menu-item>
+          <el-icon><IEpSetting /></el-icon> <span> 广告位管理 </span>
+        </el-menu-item>
+      </el-sub-menu>
+    </el-menu>
+  </el-aside>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.el-aside {
+  background-color: #eee;
+  height: 100vh;
+  width: auto;
+}
+.el-menu {
+  background-color: #eee;
+  width: 200px;
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,41 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      imports: ['vue'],
+      resolvers: [
+        ElementPlusResolver(),
+
+        // 自动导入图标组件
+        IconsResolver({
+          prefix: 'Icon'
+        })
+      ]
+    }),
+    Components({
+      resolvers: [
+        ElementPlusResolver(),
+
+        // 自动注册图标组件
+        IconsResolver({
+          enabledCollections: ['ep']
+        })
+      ]
+    }),
+    Icons({
+      autoInstall: true
+    })
   ],
   resolve: {
     alias: {
