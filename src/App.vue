9407353355e8aed0c1954f17f53f4bf7cccc0161
luoyulong100 <17745132831@163.com>
2024-02-29 23:24:50 +0800

    feat：接口测试

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import request from './utils/request'
+
+request({
+  method: 'GET',
+  url: '/front/ad/getAdList'
+}).then((res) => {
+  console.log('res---', res)
+})
 </script>
 
 <template>
